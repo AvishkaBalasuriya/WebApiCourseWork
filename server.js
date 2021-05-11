b18970a0311b0728e33617bb92df189a0eed5a6d
AvishkaBalasuriya <avishkabalasuriya980330@gmail.com>
2021-05-11 20:38:27 +0530

    React frontend added

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/v1/otp',otpRoute)
 app.use('/api/v1/products',productRoute)
 app.use('/api/v1/vendor',vendorRoute)
 
-app.get("/admin", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
