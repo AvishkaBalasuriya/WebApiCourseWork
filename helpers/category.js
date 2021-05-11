f89089e83ef0d81edb9526f1af86bae56bccc7e3
AvishkaBalasuriya <avishkabalasuriya980330@gmail.com>
2021-05-11 20:56:34 +0530

    Admin registration seperated

--- helpers/category.js
+++ helpers/category.js
@@ -1,104 +1,26 @@
-const codeGenerator = require('../utils/codeGenerator')
-const validators = require('../utils/validators')
-const time = require('../utils/time')
+const masterCategoryModel = require('../models/masterCategory').MasterCategory
+const subCategoryModel = require('../models/subCategory').SubCategory
 
-const sms = require('../services/sms')
-const email = require('../services/email')
-
-const userModel = require('../models/user')
-const otpModel = require('../models/otp')
-
-function getAll(contact,contactType){
+function getAll(){
     return new Promise(async(resolve,reject)=>{
         try{
-            if(contactType){
-                var user = await userModel.User.findOne({mobileNumber:contact})
-            }else{
-                var user = await userModel.User.findOne({email:contact})
-            }
-
-            if(!user)
-                return reject("Contact information is not match with any of our users")
-    
-            let code = codeGenerator.generateOtp()
-    
-            let otpDetails = otpModel.Otp({user:user,code:code,sentTo:contactType})
-    
-            let messageContent = "Please use below OTP to reset your password. \n "+code.toString()
-    
-            if(contactType===1){
-                await sms.sendSms(contact,messageContent).catch((error)=>{
-                    return reject(error)
-                })
-            }else{
-                await email.sendEmail(contact,"Verify OTP",messageContent).catch((error)=>{
-                    return reject(error)
-                })
-            }
-    
-            otpDetails.save().then((res)=>{
-                let data = {
-                    userId:user._id
-                }
+            let categories = await masterCategoryModel.find().populate('subCategory')
         
-                return resolve(data)
-            }).catch((e)=>{return reject("Unable to save otp details")})
-    
+            resolve(categories)
         }catch(e){
             return reject(e.message)
         }
     })
 }
 
-function addNewMasterCategory(otp,userId){
+function addNewMasterCategory(name){
     return new Promise(async(resolve,reject)=>{
         try{
-            let otpDetails = await otpModel.Otp.findOne({user:new userModel.mongoose.Types.ObjectId(userId),code:otp})
+            let masterCategory = new masterCategoryModel.MasterCategory({
+                name:name
+            })
 
-            if(!otpDetails)
-                return reject("Invalid OTP code")
-            
-            let user = await userModel.User.findOne({_id:new userModel.mongoose.Types.ObjectId(userId)})
-
-            if(otpDetails.isVerified)
-                return reject("This OTP code is already verified")
-
-            let timeAfterOtpIssued = time.calculateTimeDifferent(otpDetails.createdAt,new Date())
-
-            if(timeAfterOtpIssued>=120)
-                return reject("OTP code expired. Please try with new OTP")
-
-            if(user.status!=1){
-                user.status=1
-
-                otpDetails.isVerified = true
-                otpDetails.isActive = false
-
-                await user.save()
-                await otpDetails.save()
-
-                let data = {
-                    userId:otpDetails.user._id
-                }
-                
-                return resolve(data)
-            }
-            
-            let uuid = codeGenerator.generateUUID()
-
-            otpDetails.isVerified = true
-            otpDetails.isActive = true
-            otpDetails.uuid = uuid
-
-            otpDetails.save().then((res)=>{
-                let data = {
-                    userId:otpDetails.user._id,
-                    otpId:uuid
-                }
-            
-                return resolve(data)
-            }).catch((e)=>{return reject("Unable to save otp details")})
-            
+            let result = await masterCategory()
         }catch(e){
             return reject(e.message)
         }
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const login = require('../auth/login')
 const register = require('../auth/register')
 const forget = require('../auth/forget')
 
+const jwtMiddleware = require('../middlewares/jwt').checkJWT
+const checkAdminPermissions = require('../middlewares/permissionCheck').checkAdminPermissions
+
 const validator = require('../utils/validators')
 
 module.exports = (()=>{
@@ -63,6 +66,47 @@ module.exports = (()=>{
                 return respond.status(200).send({success:true,message:'User successfully registered',error:null,data:result})
             }).catch((error)=>{
                 return respond.status(200).send({success:false,message:'Registration failed',error:error,data:null})
+            })
+        }catch(e){
+            return respond.status(500).send({success:false,message:'Unexpected error occurs',error:e.message,data:null})
+        }
+    })
+
+    routes.post('/register/admin',jwtMiddleware,checkAdminPermissions,(request, respond)=>{
+        try{
+
+            let email = request.body.email
+            let password = request.body.password
+            let passwordConfirm = request.body.passwordConfirm
+            let firstName = request.body.firstName
+            let lastName = request.body.lastName
+            let mobileNumber = request.body.mobileNumber
+            let address = request.body.address
+            let isSocial = request.body.isSocial
+            let type = 1
+
+            if(!validator.validateEmptyFields(email,password,passwordConfirm,firstName,lastName,address,isSocial))
+                return respond.status(200).send({success:false,message:'Missing or empty required fields',error:null,data:null})
+
+            if(!validator.validateEmail(email))
+                return respond.status(200).send({success:false,message:'Provided email is not valid',error:null,data:null})
+
+            if(mobileNumber && !validator.validateMobileNumber(mobileNumber))
+                return respond.status(200).send({success:false,message:'Provided mobile number is not valid',error:null,data:null})
+            
+            if(!validator.validateConfirmPassword(password,passwordConfirm))
+                return respond.status(200).send({success:false,message:'Passwords not matching',error:null,data:null})
+    
+            if(!validator.validatePassword(password))
+                return respond.status(200).send({success:false,message:'Password mot matching security criteria',error:null,data:null})
+    
+            let user = new userModel.User({email:email,rawPassword:password,firstName:firstName,
+                lastName:lastName,mobileNumber:mobileNumber,address:address,isSocial:isSocial,type:type})
+            
+            register(user).then((result)=>{
+                return respond.status(200).send({success:true,message:'Admin User successfully registered',error:null,data:result})
+            }).catch((error)=>{
+                return respond.status(200).send({success:false,message:'Admin Registration failed',error:error,data:null})
             })
         }catch(e){
             return respond.status(500).send({success:false,message:'Unexpected error occurs',error:e.message,data:null})
